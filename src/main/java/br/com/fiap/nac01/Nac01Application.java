edf083bc904fa51847dfc44232be51201de888c3
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2020-09-09 20:37:57 -0300

    Adicionar projeto Spring Initializr

---
+++ src/main/java/br/com/fiap/nac01/Nac01Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.nac01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nac01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nac01Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/nac01/Nac01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.nac01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nac01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
