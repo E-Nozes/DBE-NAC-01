b0d0b42fa1b3aa2e53dec9e587d35aedcff7d35d
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2020-09-09 23:37:58 -0300

    Removendo imports não utilizados

--- src/main/java/br/com/fiap/nac01/repository/AluguelRepository.java
+++ src/main/java/br/com/fiap/nac01/repository/AluguelRepository.java
@@ -3,7 +3,6 @@ package br.com.fiap.nac01.repository;
 import br.com.fiap.nac01.model.Aluguel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface AluguelRepository extends JpaRepository<Aluguel, Integer> {
